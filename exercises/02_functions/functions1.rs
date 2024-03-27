f478bd9abbfdbcfc4bba3658a3043373496c9ce6
benebobaa <bensatriya3@gmail.com>
2024-03-27 14:25:06 +0700

    learn rust

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -3,8 +3,12 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
-    call_me();
+    call_me(32);
+}
+
+
+fn call_me(num:i32){
+    println!("ini angka {}",num);
 }
